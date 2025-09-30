19aebdc6c7ab70961527e9eecfcfe6246531764f
lyula <sacredlyula@gmail.com>
2025-09-30 12:01:31 +0300

    fixed the images paths

--- client/src/components/WalletGrid.jsx
+++ client/src/components/WalletGrid.jsx
@@ -6,88 +6,162 @@ import okxImg from '../assets/images/okx.png';
 import blueWalletImg from '../assets/images/blue-wallet.png';
 import rainbowImg from '../assets/images/rainbow.png';
 import keplrImg from '../assets/images/keplr.png';
+import metamaskImg from '../assets/images/metamask.png';
+import mewImg from '../assets/images/MEW.png';
+import cryptoImg from '../assets/images/crypto.png';
+import trustWalletImg from '../assets/images/trustwallet.png';
+import walletConnectImg from '../assets/images/walletconnect-logo.svg';
+import coinbaseImg from '../assets/images/coinbase.jpg';
+import bscwImg from '../assets/images/bscw.jpg';
+import atomicImg from '../assets/images/atomic.png';
+import exodusImg from '../assets/images/exodus_wallet.png';
+import bnbImg from '../assets/images/bnb.png';
+import polkadotImg from '../assets/images/polkadot.png';
+import xrpImg from '../assets/images/xrp.png';
+import stellarImg from '../assets/images/stellar.png';
+import tezosImg from '../assets/images/tezos.png';
+import thetaImg from '../assets/images/theta.png';
+import tronImg from '../assets/images/tron.png';
+import cosmosImg from '../assets/images/cosmos.png';
+import kavaImg from '../assets/images/kava.png';
+import filecoinImg from '../assets/images/filecoin.png';
+import solanaImg from '../assets/images/solana.png';
+import venlyImg from '../assets/images/venly.svg';
+import zilliqaImg from '../assets/images/zilliqa.png';
+import elrondImg from '../assets/images/elrond.jpg';
+import wavesImg from '../assets/images/waves.png';
+import iconImg from '../assets/images/icon.png';
+import ontologyImg from '../assets/images/ontology.png';
+import oxenImg from '../assets/images/oxen.png';
+import nanoImg from '../assets/images/nano.png';
+import tomoImg from '../assets/images/tomo.png';
+import vechainImg from '../assets/images/vechain.jpg';
+import iotexImg from '../assets/images/iotex.png';
+import wanchainImg from '../assets/images/wanchain.png';
+import harmonyImg from '../assets/images/harmony.png';
+import kinImg from '../assets/images/kin.png';
+import nimiqImg from '../assets/images/nimiq.png';
+import aionImg from '../assets/images/aion.png';
+import thundertokenImg from '../assets/images/thundertoken.png';
+import aeternityImg from '../assets/images/aeternity.png';
+import nebulasImg from '../assets/images/nebulas.png';
+import fioImg from '../assets/images/fio.png';
+import gochainImg from '../assets/images/gochain.png';
+import poaImg from '../assets/images/poa.png';
+import callistoImg from '../assets/images/callisto.png';
+import bitpayImg from '../assets/images/bitpay.jpg';
+import aktionariatImg from '../assets/images/aktionariat.png';
+import atwalletImg from '../assets/images/atwallet.png';
+import guardImg from '../assets/images/guard.png';
+import aaveImg from '../assets/images/aave.jpg';
+import enjinImg from '../assets/images/enjin.jpg';
+import skaleImg from '../assets/images/skale.png';
+import portisImg from '../assets/images/portis.png';
+import trezorImg from '../assets/images/trezor.png';
+import squarelinkImg from '../assets/images/squarelink.png';
+import torusImg from '../assets/images/torus.jpg';
+import scatterImg from '../assets/images/scatter.jpg';
+import sumcoinImg from '../assets/images/sumcoin.png';
+import phantomImg from '../assets/images/phantom.png';
+import bitcoinImg from '../assets/images/bitcoin.png';
+import paxfulImg from '../assets/images/paxful.png';
+import blockchainImg from '../assets/images/blockchain.png';
+import bitgetImg from '../assets/images/bitget.png';
+import safepalImg from '../assets/images/safepal.png';
+import coinomiImg from '../assets/images/coinomi.png';
+import mathWalletImg from '../assets/images/math-wallet.png';
+import fortmaticImg from '../assets/images/fortmatic.png';
+import digitexImg from '../assets/images/digitex.png';
+import ledgerImg from '../assets/images/ledger.png';
+import defiatImg from '../assets/images/defiat.jpg';
+import authereumImg from '../assets/images/authereum.png';
+import flareImg from '../assets/images/flare.jpg';
+import zelcoreImg from '../assets/images/zelcore.png';
+import bitkeepImg from '../assets/images/bitkeep.png';
+import coin98Img from '../assets/images/coin98.png';
+import trustVaultImg from '../assets/images/trustvault.png';
 
 export const wallets = [
-  { name: 'MetaMask', image: '/src/assets/images/metamask.png' },
-  { name: 'MEW Wallet', image: '/src/assets/images/MEW.png' },
-  { name: 'Crypto.com DeFi Wallet', image: '/src/assets/images/crypto.png' },
-  { name: 'Trust Wallet', image: '/src/assets/images/trustwallet.png' },
-  { name: 'Wallet\nConnect', image: '/walletconnect-logo.svg' },
-  { name: 'Coinbase Wallet', image: '/src/assets/images/coinbase.jpg' },
-  { name: 'Binance Chain', image: '/src/assets/images/bscw.jpg' },
-  { name: 'Atomic Wallet', image: '/src/assets/images/atomic.png' },
-  { name: 'Exodus Wallet', image: '/src/assets/images/exodus_wallet.png' },
-  { name: 'BNB', image: '/src/assets/images/bnb.png' },
-  { name: 'Polkadot', image: '/src/assets/images/polkadot.png' },
-  { name: 'XRP', image: '/src/assets/images/xrp.png' },
-  { name: 'Stellar', image: '/src/assets/images/stellar.png' },
-  { name: 'Tezos', image: '/src/assets/images/tezos.png' },
-  { name: 'Theta', image: '/src/assets/images/theta.png' },
-  { name: 'Tron', image: '/src/assets/images/tron.png' },
-  { name: 'Cosmos', image: '/src/assets/images/cosmos.png' },
-  { name: 'Kava', image: '/src/assets/images/kava.png' },
-  { name: 'Filecoin', image: '/src/assets/images/filecoin.png' },
-  { name: 'Solana', image: '/src/assets/images/solana.png' },
-  { name: 'Venly', image: '/venly.svg' },
-  { name: 'Zilliqa', image: '/src/assets/images/zilliqa.png' },
-  { name: 'Elrond', image: '/src/assets/images/elrond.jpg' },
-  { name: 'Waves', image: '/src/assets/images/waves.png' },
-  { name: 'ICON', image: '/src/assets/images/icon.png' },
-  { name: 'Ontology', image: '/src/assets/images/ontology.png' },
-  { name: 'Oxen Wallet', image: '/src/assets/images/oxen.png' },
-  { name: 'Nano', image: '/src/assets/images/nano.png' },
-  { name: 'Tomo Chain', image: '/src/assets/images/tomo.png' },
-  { name: 'VeChain', image: '/src/assets/images/vechain.jpg' },
-  { name: 'IoTex', image: '/src/assets/images/iotex.png' },
-  { name: 'Wanchain', image: '/src/assets/images/wanchain.png' },
-  { name: 'Harmony', image: '/src/assets/images/harmony.png' },
-  { name: 'Kin', image: '/src/assets/images/kin.png' },
-  { name: 'Nimiq', image: '/src/assets/images/nimiq.png' },
-  { name: 'Aion', image: '/src/assets/images/aion.png' },
-  { name: 'Thunder Token', image: '/src/assets/images/thundertoken.png' },
-  { name: 'Aeternity', image: '/src/assets/images/aeternity.png' },
-  { name: 'Nebulas', image: '/src/assets/images/nebulas.png' },
-  { name: 'FIO', image: '/src/assets/images/fio.png' },
-  { name: 'GoChain', image: '/src/assets/images/gochain.png' },
-  { name: 'POA Network', image: '/src/assets/images/poa.png' },
-  { name: 'Callisto', image: '/src/assets/images/callisto.png' },
-  { name: 'BitPay', image: '/src/assets/images/bitpay.jpg' },
-  { name: 'Aktionariat', image: '/src/assets/images/aktionariat.png' },
-  { name: 'AtWallet', image: '/src/assets/images/atwallet.png' },
-  { name: 'Guard Wallet', image: '/src/assets/images/guard.png' },
-  { name: 'AAVE', image: '/src/assets/images/aave.jpg' },
-  { name: 'Enjin', image: '/src/assets/images/enjin.jpg' },
-  { name: 'Skale', image: '/src/assets/images/skale.png' },
-  { name: 'Portis', image: '/src/assets/images/portis.png' },
-  { name: 'Trezor', image: '/src/assets/images/trezor.png' },
-  { name: 'Squarelink', image: '/src/assets/images/squarelink.png' },
-  { name: 'Torus', image: '/src/assets/images/torus.jpg' },
-  { name: 'Scatter Wallet', image: '/src/assets/images/scatter.jpg' },
-  { name: 'Sumcoin', image: '/src/assets/images/sumcoin.png' },
-  { name: 'Phantom', image: '/src/assets/images/phantom.png' },
-  { name: 'Electrum', image: '/src/assets/images/electrum.png' },
-  { name: 'Bitcoin', image: '/src/assets/images/bitcoin.png' },
-  { name: 'Blue Wallet', image: 'src/assets/images/blue-wallet.png' },
-  { name: 'OKX', image: 'src/assets/images/okx.png' },
-  { name: 'Paxful', image: '/src/assets/images/paxful.png' },
-  { name: 'Blockchain.com', image: '/src/assets/images/blockchain.png' },
-  { name: 'Bitget', image: '/src/assets/images/bitget.png' },
-  { name: 'Safepal', image: '/src/assets/images/safepal.png' },
+  { name: 'MetaMask', image: metamaskImg },
+  { name: 'MEW Wallet', image: mewImg },
+  { name: 'Crypto.com DeFi Wallet', image: cryptoImg },
+  { name: 'Trust Wallet', image: trustWalletImg },
+  { name: 'Wallet\nConnect', image: walletConnectImg },
+  { name: 'Coinbase Wallet', image: coinbaseImg },
+  { name: 'Binance Chain', image: bscwImg },
+  { name: 'Atomic Wallet', image: atomicImg },
+  { name: 'Exodus Wallet', image: exodusImg },
+  { name: 'BNB', image: bnbImg },
+  { name: 'Polkadot', image: polkadotImg },
+  { name: 'XRP', image: xrpImg },
+  { name: 'Stellar', image: stellarImg },
+  { name: 'Tezos', image: tezosImg },
+  { name: 'Theta', image: thetaImg },
+  { name: 'Tron', image: tronImg },
+  { name: 'Cosmos', image: cosmosImg },
+  { name: 'Kava', image: kavaImg },
+  { name: 'Filecoin', image: filecoinImg },
+  { name: 'Solana', image: solanaImg },
+  { name: 'Venly', image: venlyImg },
+  { name: 'Zilliqa', image: zilliqaImg },
+  { name: 'Elrond', image: elrondImg },
+  { name: 'Waves', image: wavesImg },
+  { name: 'ICON', image: iconImg },
+  { name: 'Ontology', image: ontologyImg },
+  { name: 'Oxen Wallet', image: oxenImg },
+  { name: 'Nano', image: nanoImg },
+  { name: 'Tomo Chain', image: tomoImg },
+  { name: 'VeChain', image: vechainImg },
+  { name: 'IoTex', image: iotexImg },
+  { name: 'Wanchain', image: wanchainImg },
+  { name: 'Harmony', image: harmonyImg },
+  { name: 'Kin', image: kinImg },
+  { name: 'Nimiq', image: nimiqImg },
+  { name: 'Aion', image: aionImg },
+  { name: 'Thunder Token', image: thundertokenImg },
+  { name: 'Aeternity', image: aeternityImg },
+  { name: 'Nebulas', image: nebulasImg },
+  { name: 'FIO', image: fioImg },
+  { name: 'GoChain', image: gochainImg },
+  { name: 'POA Network', image: poaImg },
+  { name: 'Callisto', image: callistoImg },
+  { name: 'BitPay', image: bitpayImg },
+  { name: 'Aktionariat', image: aktionariatImg },
+  { name: 'AtWallet', image: atwalletImg },
+  { name: 'Guard Wallet', image: guardImg },
+  { name: 'AAVE', image: aaveImg },
+  { name: 'Enjin', image: enjinImg },
+  { name: 'Skale', image: skaleImg },
+  { name: 'Portis', image: portisImg },
+  { name: 'Trezor', image: trezorImg },
+  { name: 'Squarelink', image: squarelinkImg },
+  { name: 'Torus', image: torusImg },
+  { name: 'Scatter Wallet', image: scatterImg },
+  { name: 'Sumcoin', image: sumcoinImg },
+  { name: 'Phantom', image: phantomImg },
+  { name: 'Electrum', image: electrumImg },
+  { name: 'Bitcoin', image: bitcoinImg },
+  { name: 'Blue Wallet', image: blueWalletImg },
+  { name: 'OKX', image: okxImg },
+  { name: 'Paxful', image: paxfulImg },
+  { name: 'Blockchain.com', image: blockchainImg },
+  { name: 'Bitget', image: bitgetImg },
+  { name: 'Safepal', image: safepalImg },
   { name: 'Keplr', image: keplrImg },
   { name: 'Mycelium', image: myceliumImg },
-  { name: 'Coinomi', image: '/src/assets/images/coinomi.png' },
+  { name: 'Coinomi', image: coinomiImg },
   { name: 'Rainbow Wallet', image: rainbowImg },
-  { name: 'Math Wallet', image: '/src/assets/images/math-wallet.png' },
-  { name: 'FortMatic', image: '/src/assets/images/fortmatic.png' },
-  { name: 'Digitex', image: '/src/assets/images/digitex.png' },
-  { name: 'Ledger', image: '/src/assets/images/ledger.png' },
-  { name: 'Defiat', image: '/src/assets/images/defiat.jpg' },
-  { name: 'Authereum', image: '/src/assets/images/authereum.png' },
-  { name: 'Flare Wallet', image: '/src/assets/images/flare.jpg' },
-  { name: 'Zelcore', image: '/src/assets/images/zelcore.png' },
-  { name: 'BitKeep', image: '/src/assets/images/bitkeep.png' },
-  { name: 'Coin98', image: '/src/assets/images/coin98.png' },
-  { name: 'Trust Vault', image: '/src/assets/images/trustvault.png' },
+  { name: 'Math Wallet', image: mathWalletImg },
+  { name: 'FortMatic', image: fortmaticImg },
+  { name: 'Digitex', image: digitexImg },
+  { name: 'Ledger', image: ledgerImg },
+  { name: 'Defiat', image: defiatImg },
+  { name: 'Authereum', image: authereumImg },
+  { name: 'Flare Wallet', image: flareImg },
+  { name: 'Zelcore', image: zelcoreImg },
+  { name: 'BitKeep', image: bitkeepImg },
+  { name: 'Coin98', image: coin98Img },
+  { name: 'Trust Vault', image: trustVaultImg },
 ];
 
 const WalletGrid = ({ wallets }) => {
